3083451c0fbb472f9592a88150467398b3dd3dd1
GuestAUser <62957566+GuestAUser@users.noreply.github.com>
2020-04-07 17:07:28 -0300

    Challenge CyberSecurity
    Ultra easy challenge for those who want to see the piece of what's hacking.

---
+++ challenge/Mycode.js
@@ -0,0 +1,51 @@
+//code made by: Davi Chaves Azevedo aka GuestAUser
+//github: https://github.com/GuestAUser/
+//GNU LICENSE:3.0
+//tools:
+    // HexaConverter.cpp for variables in code;(It's some what of an obfuscation but It can be repaired with any text editor)
+
+
+
+//============================================================ABOUT THE CHALLENGE============================================================
+//the challenge is to set devMode = true as guest, LOOK AT CONSOLE RESULTS!!                                                                /
+//I did not obfuscate the code because google and other browsers already have a tool to reverse that                                        /
+//So instead I did a memory manegement technique to obfuscate the boolean vars using pointers instead [look at tools for hex vars]          /
+//If you find a deno(nodejs 2.0) server file> ITS NOT PART OF THE CODE, ITS FOR MEMORY COMPATIBILIY, changing it will not affect the result /
+//deno file is as .ts(typescript) in case you don't know                                                                                    /
+//===========================================================================================================================================
+
+
+const _0x4992 = ['ERROR\x20AT\x20EXECUTION\x20OF\x20INDEX', 'log', 'Devmode:\x20', 'Welcome\x20back\x20David!'];
+(function(_0x24e75a, _0x499212) {
+    const _0xc0d49e = function(_0x49a619) {
+        while (--_0x49a619) {
+            _0x24e75a['push'](_0x24e75a['shift']());
+        }
+    };
+    _0xc0d49e(++_0x499212);
+}(_0x4992, 0xff));
+const _0xc0d4 = function(_0x24e75a, _0x499212) {
+    _0x24e75a = _0x24e75a - 0x0;
+    let _0xc0d49e = _0x4992[_0x24e75a];
+    return _0xc0d49e;
+};
+function index() {
+    let _0xb247e8;
+    console[_0xc0d4('0x2')]('website\x20started...');
+    let _0x3d6547 = confirm('Are\x20you\x20a\x20developer?');
+    if (_0x3d6547 == !![]) {
+        alert(_0xc0d4('0x0'));
+        _0xb247e8 = !![];
+        console[_0xc0d4('0x2')](_0xc0d4('0x3') + _0xb247e8);
+    } else {
+        alert('Hey\x20Guest!');
+        _0xb247e8 = ![]; 
+        console[_0xc0d4('0x2')](_0xc0d4('0x3') + _0xb247e8);
+    }
+}
+ableToStart = !![];
+if (ableToStart == !![]) {
+    index();
+} else {
+    console[_0xc0d4('0x2')](_0xc0d4('0x1'));
+}
